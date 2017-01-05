a093e7f737564265ecceeac2b11bacab45c79dd6
David J. Zielinski <djzielin@gmail.com>
2017-01-05 03:14:05 +0000

    trying sine example

--- render.cpp
+++ render.cpp
@@ -1,14 +1,37 @@
 #include <Bela.h>
+#include <cmath>
+
+float gFrequency = 440.0;
+float gPhase;
+float gInverseSampleRate;
 
 bool setup(BelaContext *context, void *userData)
 {
 	printf("setup happening");
+		gInverseSampleRate = 1.0 / context->audioSampleRate;
+	gPhase = 0.0;
+
 	return true;
 }
 
 void render(BelaContext *context, void *userData)
 {
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		float out = 0.8 * sinf(gPhase);
+		gPhase += 2.0 * M_PI * gFrequency * gInverseSampleRate;
+		if(gPhase > 2.0 * M_PI)
+			gPhase -= 2.0 * M_PI;
 
+		for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
+			// Two equivalent ways to write this code
+
+			// The long way, using the buffers directly:
+			// context->audioOut[n * context->audioOutChannels + channel] = out;
+
+			// Or using the macros:
+			audioWrite(context, n, channel, out);
+		}
+	}
 }
 
 void cleanup(BelaContext *context, void *userData)
